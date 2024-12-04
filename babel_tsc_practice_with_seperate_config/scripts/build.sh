c59b993da605e4959619d8f6d334af5ace4e5b09
PraneshBK <44346322+PraneshBalasubramaniam@users.noreply.github.com>
2024-12-04 13:14:45 +0000

    added monorepo

--- babel_tsc_practice_with_seperate_config/scripts/build.sh
+++ babel_tsc_practice_with_seperate_config/scripts/build.sh
@@ -1,9 +1,9 @@
-npx tsc
+pnpm exec tsc
 
 echo 'Compiled Typescript....'
 echo
 
-npx babel dist/tsc --out-dir dist/babel --extensions \".js,.jsx\"
+pnpm exec babel dist/tsc --out-dir dist/babel --extensions \".js,.jsx\"
 
 echo
 echo 'Compiled Babel....'
